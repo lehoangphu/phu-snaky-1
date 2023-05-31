04ae408ac8c669004ed6582abd69270186ed7be3
NinjaTyper <35751000+NinjaTyper@users.noreply.github.com>
2023-05-31 04:27:48 +0000

    implement basic pathfinding into petersnake

---
+++ pathfinding/peterpath.py
@@ -0,0 +1,95 @@
+class Location:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+    def __repr__(self) -> str:
+        retString = "(" + str(self.x) + ", " + str(self.y) + ")"
+        return retString
+    def __eq__(self, object) -> bool:
+        if self.x == object.x and self.y == object.y:
+            return True
+        else:
+            return False
+        
+class PathEntry:
+    def __init__(self, location) -> None:
+        self.location = location
+        self.history = []
+        self.cost = 0
+
+class PathFinder:
+    def __init__(self, startLoc, destination, obstacles, width, height) -> None:
+        self.data = []
+        self.startLoc = startLoc
+        self.destination = destination
+        self.obstacles = obstacles
+        self.width = width
+        self.height = height
+        self.visited = []
+    
+    def addPathData(self, oldEntry, newlocation):
+        if (self.isValidLocation(newlocation)):
+            newEntry = PathEntry(newlocation)
+            newEntry.history = oldEntry.history.copy()
+            newEntry.history.append(oldEntry.location)
+            self.data.append(newEntry)
+
+    def findthepath(self):
+        startEntry = PathEntry(self.startLoc)
+        self.data.append(startEntry)
+
+        while len(self.data) > 0:
+            current = self.data.pop(0)
+            if current.location == self.destination:
+                current.history.append(self.destination)
+                # print("Found a path")
+                return current.history
+                break
+            if not self.isVisited(current.location):
+                self.visited.append(current.location)
+                uplocation = Location(current.location.x, current.location.y+1)
+                downlocation = Location(current.location.x, current.location.y-1)
+                leftlocation = Location(current.location.x-1, current.location.y)
+                rightlocation = Location(current.location.x+1, current.location.y)
+
+                self.addPathData(current, uplocation)
+                self.addPathData(current, downlocation)
+                self.addPathData(current, leftlocation)
+                self.addPathData(current, rightlocation)
+
+    def isValidLocation(self, location):
+        if location.x < 0 or location.x > self.width-1:
+            return False
+        if location.y < 0 or location.y > self.height-1:
+            return False
+        for obstacles in self.obstacles:
+            if obstacles == location:
+                return False
+        return True
+    def isVisited(self, newlocation):
+        for location in self.visited:
+            if location == newlocation:
+                return True
+        return False
+
+def basicTest1():
+    startLoc = Location(0,0)
+    destination = Location(8,8)
+    obstacles = [
+        Location(4, 0),
+        Location(4, 1),
+        Location(4, 2),
+        Location(4, 3),
+        Location(4, 4),
+        Location(4, 5),
+        Location(4, 6),
+        Location(4, 7),
+        Location(4, 8),
+        Location(4, 9)
+    ]
+    finder = PathFinder(startLoc, destination, obstacles, 11, 11)
+    
+    finder.findthepath()
+
+if __name__ == "__main__":
+    basicTest1()
--- snake_peter.py
+++ snake_peter.py
@@ -1,5 +1,9 @@
 import random
 import typing
+from pathfinding.peterpath import Location
+from pathfinding.peterpath import PathFinder
+import json
+from pathlib import Path
 
 def info_peter():
     return {
@@ -14,91 +18,49 @@ def info_peter():
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move_peter(game_state: typing.Dict) -> typing.Dict:
-
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
-    # We've included code to prevent your Battlesnake from moving backwards
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-
-        is_move_safe["left"] = False
-
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-        is_move_safe["right"] = False
-
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-        is_move_safe["down"] = False
-
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-        is_move_safe["up"] = False
-
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
-
-    if (my_head['x'] == 0):
-        is_move_safe["left"] = False
-    if (my_head['x'] == board_width-1):
-        is_move_safe["right"] = False
-    if (my_head['y'] == 0):
-        is_move_safe["down"] = False
-    if (my_head['y'] == board_height-1):
-        is_move_safe["up"] = False
-
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     my_body = game_state['you']['body']
-    for part in my_body:
-        if part["x"] == my_head["x"]+1 and part["y"] == my_head["y"]:
-            is_move_safe["right"] = False
-        if part["x"] == my_head["x"]-1 and part["y"] == my_head["y"]:
-            is_move_safe["left"] = False
-        if part["y"] == my_head["y"]+1 and part["x"] == my_head["x"]:
-            is_move_safe["up"] = False
-        if part["y"] == my_head["y"]-1 and part["x"] == my_head["x"]:
-            is_move_safe["down"] = False
-
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     opponents = game_state['board']['snakes']
-    for snake in opponents:
-        for part in snake["body"]:
-            if part["x"] == my_head["x"]+1 and part["y"] == my_head["y"]:
-                is_move_safe["right"] = False
-            if part["x"] == my_head["x"]-1 and part["y"] == my_head["y"]:
-                is_move_safe["left"] = False
-            if part["y"] == my_head["y"]+1 and part["x"] == my_head["x"]:
-                is_move_safe["up"] = False
-            if part["y"] == my_head["y"]-1 and part["x"] == my_head["x"]:
-                is_move_safe["down"] = False
 
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
-
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # Step 4 - Move towards food instead of random, to regain health and survive longer
-    # TODO: Need to run to the nearest food
     food = game_state['board']['food']
     if (len(food) > 0):
         first_food = food[0]
-        if (first_food['x'] < my_head["x"] and is_move_safe["left"]):
-            return {"move": "left"}
-        elif (first_food['x'] > my_head["x"] and is_move_safe["right"]):
-            return {"move": "right"}
-        elif (first_food['y'] < my_head["y"] and is_move_safe["down"]):
-            return {"move": "down"}
-        elif (first_food['y'] > my_head["y"] and is_move_safe["up"]):
-            return {"move": "up"}
-    
 
-    print(f"MOVE {game_state['turn']}: {next_move}")
-    return {"move": next_move}
+
+    startLoc = Location(my_head["x"],my_head["y"])
+    destination = Location(first_food["x"],first_food["y"])
+    obstacles = []
+    for snake in opponents:
+        for position in snake["body"]:
+            obstacles.append(Location(position["x"],position["y"]))
+    
+    for part in my_body:
+        obstacles.append(Location(part["x"], part["y"]))
+    
+    print("obstacles: ", obstacles)
+    finder = PathFinder(startLoc, destination, obstacles, board_width, board_height)
+    
+    path = finder.findthepath()
+    print("path: ", path)
+    current = path[0]
+    nextmove = path[1]
+    if nextmove.x > current.x:
+        return {"move":"right"}
+    if nextmove.x < current.x:
+        return {"move":"left"}
+    if nextmove.y > current.y:
+        return {"move":"up"}
+    if nextmove.y < current.y:
+        return {"move":"down"}
+
+    return {"move": "down"}
+
+if __name__ == "__main__":
+    dataFilePath = Path(__file__).parent / "snake_peter.json"
+    rhandle = open(dataFilePath, "r")
+
+    gamestate = json.load(rhandle)
+    rhandle.close()
+    print(move_peter(gamestate))
